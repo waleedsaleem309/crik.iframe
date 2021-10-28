3f5edf29265f389183d178f150354f5f08e1f4ed
waleedsaleem309 <70096047+waleedsaleem309@users.noreply.github.com>
2021-10-28 13:37:23 +0500

    Update ptv.js

--- ptv-sports/ptv.js
+++ ptv-sports/ptv.js
@@ -1,1 +1,2 @@
-document.write('<ifr'+'ame src="//www.pkcast123.me/embed2.php?player='mobile'&live='+ fid +'&vw='+v_width+'&vh='+v_height+'" width='+ v_width +' height=' + v_height + ' scrolling=no frameborder=0 scrolling=no allowtransparency=true id=thatframe  webkitallowfullscreen mozallowfullscreen allowfullscreen ></ifr'+'ame>') ;
+var embedded = "mobile";
+document.write('<ifr'+'ame src="//www.pkcast123.me/embed2.php?player='+ embedded +'&live='+ fid +'&vw='+v_width+'&vh='+v_height+'" width='+ v_width +' height=' + v_height + ' scrolling=no frameborder=0 scrolling=no allowtransparency=true id=thatframe  webkitallowfullscreen mozallowfullscreen allowfullscreen ></ifr'+'ame>') ;
