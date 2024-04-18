81d197311ed87ff1eae01a4e10352769700da797
waleedsaleem309 <70096047+waleedsaleem309@users.noreply.github.com>
2024-04-18 17:12:45 +0500

    Update d.php

--- ptv-sports/d.php
+++ ptv-sports/d.php
@@ -10,6 +10,5 @@
           html, body {margin:0;padding:0;height:100%;}
     </style>
  <script>fid="ptvpk"; v_width="100%"; v_height="450";</script><script type="text/javascript" src="//focus4ca.com/embedcr.js"> 
-    </iframe>
   </body>
     </html>
