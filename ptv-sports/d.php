53f6296c0c6a91d965a09c1fad7e1a0e94b37582
waleedsaleem309 <70096047+waleedsaleem309@users.noreply.github.com>
2024-04-18 17:12:22 +0500

    Update and rename d.html to d.php

---
+++ ptv-sports/d.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+ 
+ 
+    <title>PTV Sports</title>
+  </head>
+  <body>
+    <style>
+          html, body {margin:0;padding:0;height:100%;}
+    </style>
+ <script>fid="ptvpk"; v_width="100%"; v_height="450";</script><script type="text/javascript" src="//focus4ca.com/embedcr.js"> 
+    </iframe>
+  </body>
+    </html>
